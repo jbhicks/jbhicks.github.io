9293f7d0f76348b4e727f7b8f701e133042bf23e
Joshua Hicks <joshua.brock.hicks@gmail.com>
2023-09-08 14:03:46 -0700

    first test

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-static";
+// was "@sveltejs/adapter-auto"
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = "production" === "development";
+
+/** @type {import(""@sveltejs/kit").Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: "docs",
+			assets: "docs",
+			strict: false
+		}),
+		paths: {
+			// change below to your repo name
+			base: dev ? "" : "",
+		},
 	}
 };
 
